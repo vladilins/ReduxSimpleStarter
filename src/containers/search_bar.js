702f680869fbade8959a6df0beb466cb407f5e36
vladilin <ilin96inbox@gmail.com>
2020-06-15 21:19:01 +0900

    inpute state

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,13 +2,33 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 export class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
       <div>
-        <form action="" className="input-group">
-          <input type="text" />
+        <form className="input-group">
+          <input
+            type="text"
+            placeholder="Get a five-day forecast in your favorite cities"
+            className="form-control"
+            value={this.state.term}
+            onChange={this.onInputChange}
+          />
           <span className="input-group-btn">
-            <button className="btn btn-secondary">Submit</button>
+            <button type="submit" className="btn btn-secondary">
+              Submit
+            </button>
           </span>
         </form>
       </div>
