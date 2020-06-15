dc645ba7c1f914918e70aebe6c43398d206424f5
vladilin <ilin96inbox@gmail.com>
2020-06-15 21:44:27 +0900

    fetch action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from "axios";
 export const API_KEY = "306f4fc600a287b56b34cf5b083e8a87";
+export const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export const fetchWeather = (city) => {
+  const url = `${ROOT_URL}&q=${city}`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+};
