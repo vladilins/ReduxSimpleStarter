e7eaa5d47431961d1b7400fed0ca1f076a8c04b4
vladilin <ilin96inbox@gmail.com>
2020-06-16 12:03:03 +0900

    remove key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_KEY = "306f4fc600a287b56b34cf5b083e8a87";
+export const API_KEY = "add key";
 export const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
